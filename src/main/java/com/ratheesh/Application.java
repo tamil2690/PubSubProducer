e563c45e58fb26907be4701360a3bee03666abf0
Ratheesh Sakthivel <ratheesh.sakthivel@equifax.com>
2018-11-22 14:33:32 +0000

    Pub sub producer

---
+++ src/main/java/com/ratheesh/Application.java
@@ -0,0 +1,43 @@
+package com.ratheesh.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.gcp.pubsub.core.PubSubOperations;
+import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.MessageHandler;
+public class Application {
+
+
+
+    @SpringBootApplication
+    public class Application extends SpringBootServletInitializer {
+
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(Application.class);
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(Application.class, args);
+        }
+
+        @Bean
+        @ServiceActivator(inputChannel = "pubsubOutputChannel")
+        public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+            return new PubSubMessageHandler(pubsubTemplate, "cardmovements");
+        }
+
+        @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
+        public interface PubsubOutboundGateway {
+
+            void sendToPubsub(String text);
+        }
+    }
+
+
+}
---
+++ src/main/java/com/ratheesh/producer/PubSubProducer.java
@@ -0,0 +1,5 @@
+package com.ratheesh.producer;
+
+public class PubSubProducer {
+
+}
